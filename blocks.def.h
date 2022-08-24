bf8150a147feb42aedb9c1c2ce583ba296d91efe
creator54 <hi.creator54@gmail.com>
2022-08-25 02:14:17 +0530

    fix: brightness %

--- blocks.def.h
+++ blocks.def.h
@@ -6,7 +6,7 @@ static const Block blocks[] = {
   //{"^c#3b414d^^c#7aa2f7^ "   , "printf '%s USDT\n' $(curl -s rate.sx/1btc | cut -d '.' -f1)"   			  , 		5, 			  0},
   {"^c#3b414d^^c#7aa2f7^"       , "fish -c 'audio'"   					        , 		2, 			  0},
   {"^c#3b414d^^b#7ec7a2^ "      , "fish -c 'battery fancy|cut -d':' -f1,2,3'"   , 		2, 			  0},
-  {"^c#1e222a^^b#70A1C1^  "    , "echo $(light|cut -d'.' -f1)%"                , 		0, 			 10}, //this signal is used as pkill -RTMIN+10 dwmblocks
+  {"^c#1e222a^^b#70A1C1^  "    , "fish -c 'echo (math -s0 (light))%'"          , 		0, 			 10}, //this signal is used as pkill -RTMIN+10 dwmblocks
   {"^c#3b414d^^b#7ec7a2^  "    , "fish -c ram"                                 , 		2, 			  0},
   {"^c#1e222a^^b#70A1C1^  "    , "fish -c cpu" 						        , 		2, 			  0},
   {"^c#1e222a^^b#668ee3^ 羽 "   , "date '+%a %b %d, %r '" 				        ,		1, 			  0},
