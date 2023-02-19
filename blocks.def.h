1ca626b48fd3048793fbebcd615c1efff701ff91
creator54 <hi.creator54@gmail.com>
2023-02-19 16:01:13 +0530

    cleanup

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/					    /*Command*/							            /*Update Interval*/ /*Update Signal*/
-  {"^c#3b414d^^c#7aa2f7^"   	, "fish -c 'net wlp2s0'"   			            , 		1, 			  0},
+  {"^c#3b414d^^c#7aa2f7^"   	, "net $(wifiInterface)"   			                  , 		1, 			  0},
   //is slow to update
   //{"^c#3b414d^^c#7aa2f7^ "   , "printf '%s USDT\n' $(curl -s rate.sx/1btc | cut -d '.' -f1)"   			  , 		5, 			  0},
-  {"^c#3b414d^^c#7aa2f7^"       , "fish -c 'audio'"   					        , 		2, 			  0},
-  {"^c#3b414d^^b#7ec7a2^ "      , "fish -c 'battery fancy|cut -d':' -f1,2,3'"   , 		2, 			  0},
-  {"^c#1e222a^^b#70A1C1^  "    , "fish -c 'echo (math -s0 (light))%'"          , 		0, 			 10}, //this signal is used as pkill -RTMIN+10 dwmblocks
-  {"^c#3b414d^^b#7ec7a2^  "    , "fish -c ram"                                 , 		2, 			  0},
-  {"^c#1e222a^^b#70A1C1^  "    , "fish -c cpu" 						        , 		2, 			  0},
-  {"^c#1e222a^^b#668ee3^ 羽 "   , "date '+%a %b %d, %r '" 				        ,		1, 			  0},
+  {"^c#3b414d^^c#7aa2f7^"       , "audio"       					                      , 		1, 			  0},
+  {"^c#3b414d^^b#7ec7a2^ "      , "battery fancy"                               , 		1, 			  0},
+  {"^c#1e222a^^b#70A1C1^  "    , "echo $(light | cut -d'.' -f1)%"              , 		0, 			 10}, //this signal is used as pkill -RTMIN+10 dwmblocks
+  {"^c#3b414d^^b#7ec7a2^  "    , "ram"                                         , 		2, 			  0},
+  {"^c#1e222a^^b#70A1C1^  "    , "cpu" 						                            , 		2, 			  0},
+  {"^c#1e222a^^b#668ee3^ 羽 "   , "date '+%a %b %d, %r '" 				              ,		  2, 		    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
